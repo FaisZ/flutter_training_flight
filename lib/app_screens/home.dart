accb7f6b5d3122181c67cdb19a8eb1fea9ebf404
FaisAzif <faisz9999@gmail.com>
2021-06-21 11:37:17 +0700

    3rd lesson
    Row, column, expanded widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,14 +5,64 @@ class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context){
 
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.deepPurple,
-      child: Text(
-        "Flight",
-        textDirection:
-        TextDirection.ltr,
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        //width: 200,
+        //height: 100,
+        // margin: EdgeInsets.all(15.0),
+        padding: EdgeInsets.only(left: 10, top: 40),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                firstText("Spice Jet"),
+                secondText("From Mumbai to Bangalore"),
+              ],
+            ),
+            Row(
+              children: [
+                firstText("Air India"),
+                secondText("From Jaipur to Goa"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  Expanded firstText(text) {
+    return Expanded(
+      child: Text(
+          text,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 35,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w700,
+            color: Colors.white
+          ),
+        ),
+    );
+  }
+
+  Expanded secondText(text) {
+    return Expanded(
+      child: Text(
+        text,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 20,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w700,
+            color: Colors.white
+        ),
+      ),
+    );
+  }
+
 }
